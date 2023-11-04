735a2e46dbf3d32b9c3b08367c37dba75a1df9f3
Salman <salmanhammed77@gmail.com>
2023-11-04 09:03:06 +0530

    feat: basic tictactoe

--- App.js
+++ App.js
@@ -1,20 +1,115 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
-export default function App() {
+const TicTacToe = () => {
+  const [currentPlayer, setCurrentPlayer] = useState('X');
+  const [board, setBoard] = useState(Array(9).fill(null));
+
+  const handleMove = (index) => {
+    if (board[index] || calculateWinner(board)) {
+      return;
+    }
+
+    const newBoard = [...board];
+    newBoard[index] = currentPlayer;
+    setBoard(newBoard);
+
+    setCurrentPlayer(currentPlayer === 'X' ? 'O' : 'X');
+  };
+
+  const calculateWinner = (squares) => {
+    const winningCombos = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < winningCombos.length; i++) {
+      const [a, b, c] = winningCombos[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+
+    return null;
+  };
+
+  const renderSquare = (index) => (
+    <TouchableOpacity style={getStyle(index)} onPress={() => handleMove(index)}>
+      <Text style={styles.squareText}>{board[index]}</Text>
+    </TouchableOpacity>
+  );
+
+  const getStyle = (index) => {
+    const style = [styles.square];
+    if (index % 3 === 2) {
+      style.push({ borderRightWidth: 0 });
+    }
+    if (Math.floor(index / 3) === 2) {
+      style.push({ borderBottomWidth: 0 });
+    }
+    return style;
+  };
+
+  const winner = calculateWinner(board);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.header}>{winner ? `Winner: ${winner}` : `Next Player: ${currentPlayer}`}</Text>
+      <View style={styles.board}>
+        <View style={styles.row}>
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </View>
+        <View style={styles.row}>
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </View>
+        <View style={styles.row}>
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </View>
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  header: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  board: {},
+  row: {
+    flexDirection: 'row',
+  },
+  square: {
+    width: 50,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderLeftWidth: 0,
+    borderTopWidth: 0
+  },
+  squareText: {
+    fontSize: 20,
+    fontWeight: 'bold',
   },
 });
+
+export default TicTacToe;
